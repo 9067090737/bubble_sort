ec52c1b1d2ffa859d3996768c3c455d3799f8000
9067090737 <alalvasilyeva@gmail.ru>
2013-04-09 16:12:45 +0400

    Update b_sort.cpp

--- b_sort.cpp
+++ b_sort.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int array[100];
 
-void bubble_sort(int size){ ///@function bubble_sort функция для сортировки массива пузырьком
+void bubble_sort(int size){ ///@function bubble_sort to sort array by "bubble" method
 int temp=0;
 int i,j;
 for (i=0; i<size-1; i++){
-		for (j=i+1; j<size; j++){ ///@brief внешний и внутренний циклы имеют разные параметры!!!
+		for (j=i+1; j<size; j++){ ///@brief external and internal cycles have different parameters
 			if (array[i]>array[j]){
 				temp=array[j];
 				array[j]=array[i];
@@ -16,20 +16,20 @@ for (i=0; i<size-1; i++){
 }
 
 };
-void input(int size){ ///@function input функция для ввода массива
-	int i;
+void input(int size){ ///@function input to enter elements of the array 
+int i;
 	cout<<"Input array:"<<'\n';
 	for (i=0; i<size; i++)
 		cin>>array[i];
 };
-void output(int size){ ///@function output функция для вывода массива
+void output(int size){ ///@function output to show elements of the array on the screen 
 	int i;
 	for (i=0; i<size; i++)
 		cout<<array[i]<<'\t';
 		cout<<endl;
 };
 int main(){
-	int size; ///@param size размер массива
+	int size; ///@param size to indicate size of the array
 	cout<<"Input size of array n=";
 	cin>>size;
 	cout<<'\n';
